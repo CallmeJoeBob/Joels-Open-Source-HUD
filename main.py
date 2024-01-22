5fbfcd67fd6e01de3c3525dbdc9e48a6b15d070f
Joel Joseprabu <joel.joseprabu@gmail.com>
2024-01-21 19:51:01 -0800

    Add battery percentage

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 # Python application to run Joel's Open Source HUD (Josh)
 
-# Import all board pins.
+# Import the pins for i2c bus 0
 from board import D0, D1
 import busio
-
-# Import the SSD1306 module.
 import adafruit_ssd1306
+import socket
 
-
-# Create the I2C interface.
+# Initialize display
 i2c = busio.I2C(D1, D0)
-
-# Create the SSD1306 OLED class.
-# The first two parameters are the pixel width and pixel height.  Change these
-# to the right size for your display!
 display = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
-# Alternatively you can change the I2C address of the device with an addr parameter:
-#display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x31)
+
+#Initialize socket server for battery
+host = "127.0.0.1"
+port = 8423
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host,port))
 
 # Clear the display.  Always call show after changing pixels to make the display
 # update visible!
-display.fill(0)
+def clear_display():
+  display.fill(0)
+  display.show()
 
-display.show()
+# Show battery
+def write_battery():
+  client.send(('get battery').encode())
+  response = client.recv(4096)
+  display.text(response, 0, 0)
+  display.show()
 
-# Set a pixel in the origin 0,0 position.
-display.pixel(0, 0, 1)
-# Set a pixel in the middle 64, 16 position.
-display.pixel(64, 16, 1)
-# Set a pixel in the opposite 127, 31 position.
-display.pixel(127, 31, 1)
-display.show()
+# Start main
+clear_display()
+write_battery()
